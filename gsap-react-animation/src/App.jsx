0e2ce6d1bc4c05acdffbc5bbff93742cc3b5acba
anasfaz <anaspkmmc@gmail.com>
2024-03-16 20:08:20 +0530

    new react gsap way implemantion change :refactore code

--- gsap-react-animation/src/App.jsx
+++ gsap-react-animation/src/App.jsx
@@ -1,45 +1,95 @@
 import { useRef, useLayoutEffect } from "react";
 import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import "./App.css";
 
 function App() {
   const container = useRef(null);
-  useLayoutEffect(() => {
-    let context = gsap.context(() => {
-      const t1 = gsap.timeline()
-      t1.from('#intro-slide', {
-        xPercent: "-100",
-        duration: 1.3,
-        delay: 0.3,
-      }).from(['#title-1','#title-2','#title-3'], {
-        opacity: 0,
-        xPercent: "-40",
-        duration:1,
-        y: '=+30',
-        stagger:0.5,
-      }).to(['#title-1', '#title-2', '#title-3'], {
-        opacity: 0,
-        y: '-=30',
-        delay: 0.3,
-        stagger:0.5,
-      }).to('#intro-slide', {
-        xPercent: '-100',
-        duration:1.3,
-      })
-        .from('#welcome', {
-        opacity: 0,
-        duration:0.5,
-      })
-    }, container);
-    return () => context.revert();
+  useGSAP(
+    () => {
+      gsap
+        .timeline()
+        .from("#intro-slide", {
+          xPercent: "-100",
+          duration: 1.3,
+          delay: 0.3,
+        })
+        .from(["#title-1", "#title-2", "#title-3"], {
+          opacity: 0,
+          xPercent: "-40",
+          duration: 1,
+          y: "=+30",
+          stagger: 0.5,
+        })
+        .to(["#title-1", "#title-2", "#title-3"], {
+          opacity: 0,
+          y: "-=30",
+          delay: 0.3,
+          stagger: 0.5,
+        })
+        .from("#intro-slide2", {
+          xPercent: "-100",
+          duration: 1.3,
+          delay: 0.3,
+        })
+        .to("#intro-slide", {
+          xPercent: "-100",
+          duration: 1.3,
+        })
+        .from("#welcome", {
+          opacity: 0,
+          duration: 0.5,
+        });
+    },
+    { scope: container }
+  );
 
-  }, []);
+  // this old way of approach
+  
+  // useLayoutEffect(() => {
+  //   let context = gsap.context(() => {
+  //     const t1 = gsap.timeline();
+  //     t1.from("#intro-slide", {
+  //       xPercent: "-100",
+  //       duration: 1.3,
+  //       delay: 0.3,
+  //     })
+  //       .from(["#title-1", "#title-2", "#title-3"], {
+  //         opacity: 0,
+  //         xPercent: "-40",
+  //         duration: 1,
+  //         y: "=+30",
+  //         stagger: 0.5,
+  //       })
+  //       .to(["#title-1", "#title-2", "#title-3"], {
+  //         opacity: 0,
+  //         y: "-=30",
+  //         delay: 0.3,
+  //         stagger: 0.5,
+  //       })
+  //       .from("#intro-slide2", {
+  //         xPercent: "-100",
+  //         duration: 1.3,
+  //         delay: 0.3,
+  //       })
+  //       .to("#intro-slide", {
+  //         xPercent: "-100",
+  //         duration: 1.3,
+  //       })
+  //       .from("#welcome", {
+  //         opacity: 0,
+  //         duration: 0.5,
+  //       });
+  //   }, container);
+  //   return () => context.revert();
+  // }, []);
+
   return (
     <>
       <div className="relative" ref={container}>
         <div
           id="intro-slide"
-          className="h-screen bg-gray-100 p-10 absolute top-0 left-0 font-spaceGrotesk z-10 w-full flex flex-col gap-10 tracking-tighter"
+          className="h-screen bg-gray-100 p-10 absolute top-0 left-0 font-spaceGrotesk z-11 w-full flex flex-col gap-10 tracking-tighter"
         >
           <div id="title-1" className="text-9xl ">
             software Engineer
@@ -51,8 +101,11 @@ function App() {
             freeLancer
           </div>
         </div>
-        
-        
+        <div
+          id="intro-slide2"
+          className="h-screen bg-red-500 p-10 absolute top-0 left-0 font-spaceGrotesk z-10 w-full flex flex-col gap-10 tracking-tighter"
+        ></div>
+
         <div className="h-screen flex bg-gray-950 justify-center place-items-center">
           <h1
             id="welcome"
