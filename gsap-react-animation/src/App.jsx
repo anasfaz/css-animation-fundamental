1c8d9187049a6785a4702bc59b5f6de5c5014756
anasfaz <anaspkmmc@gmail.com>
2024-03-16 06:46:08 +0530

    gsap basic animation example

--- gsap-react-animation/src/App.jsx
+++ gsap-react-animation/src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useLayoutEffect } from "react";
+import gsap from "gsap";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const container = useRef(null);
+  useLayoutEffect(() => {
+    let context = gsap.context(() => {
+      const t1 = gsap.timeline()
+      t1.from('#intro-slide', {
+        xPercent: "-100",
+        duration: 1.3,
+        delay: 0.3,
+      }).from(['#title-1','#title-2','#title-3'], {
+        opacity: 0,
+        xPercent: "-40",
+        duration:1,
+        y: '=+30',
+        stagger:0.5,
+      }).to(['#title-1', '#title-2', '#title-3'], {
+        opacity: 0,
+        y: '-=30',
+        delay: 0.3,
+        stagger:0.5,
+      }).to('#intro-slide', {
+        xPercent: '-100',
+        duration:1.3,
+      })
+        .from('#welcome', {
+        opacity: 0,
+        duration:0.5,
+      })
+    }, container);
+    return () => context.revert();
 
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="relative" ref={container}>
+        <div
+          id="intro-slide"
+          className="h-screen bg-gray-100 p-10 absolute top-0 left-0 font-spaceGrotesk z-10 w-full flex flex-col gap-10 tracking-tighter"
+        >
+          <div id="title-1" className="text-9xl ">
+            software Engineer
+          </div>
+          <div id="title-2" className="text-9xl ">
+            Designer
+          </div>
+          <div id="title-3" className="text-9xl ">
+            freeLancer
+          </div>
+        </div>
+        
+        
+        <div className="h-screen flex bg-gray-950 justify-center place-items-center">
+          <h1
+            id="welcome"
+            className="text-9xl font-bold text-gray-100 font-spaceGrotesk"
+          >
+            Welcome.
+          </h1>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
